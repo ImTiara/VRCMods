6120b0423ddabe7434defae609dcbf470086957d
Tiara <im.tiara@hotmail.com>
2021-12-12 22:27:49 +0100

    Cleanup

--- NoPerformanceStats/NoPerformanceStats.cs
+++ NoPerformanceStats/NoPerformanceStats.cs
@@ -1,8 +1,6 @@
 ﻿using HarmonyLib;
 using MelonLoader;
 using System;
-using System.Collections;
-using System.Reflection;
 using VRC.SDKBase.Validation.Performance;
 
 [assembly: MelonInfo(typeof(NoPerformanceStats.NoPerformanceStats), "NoPerformanceStats", "1.0.7", "ImTiara", "https://github.com/ImTiara/VRCMods")]
@@ -23,14 +21,14 @@ namespace NoPerformanceStats
             MelonPreferences.CreateCategory(GetType().Name, "No Performance Stats");
             MelonPreferences.CreateEntry(GetType().Name, "DisablePerformanceStats", true, "Disable Performance Stats");
 
-            ApplyPatches();
-
-            MelonCoroutines.Start(UiManagerInitializer());
-        }
-
-        private void OnUiManagerInit()
-        {
             OnPreferencesSaved();
+
+            try
+            {
+                HarmonyInstance.Patch(typeof(PerformanceScannerSet).GetMethod(nameof(PerformanceScannerSet.Method_Public_IEnumerator_GameObject_AvatarPerformanceStats_MulticastDelegateNPublicSealedBoCoUnique_0)),
+                    new HarmonyMethod(typeof(NoPerformanceStats).GetMethod("CalculatePerformance")));
+            }
+            catch (Exception e) { Logger.Error("Failed to patch Performance Scanner: " + e); }
         }
 
         public override void OnPreferencesSaved()
@@ -45,34 +43,7 @@ namespace NoPerformanceStats
             }
         }
 
-        private void ApplyPatches()
-        {
-            try
-            {
-                Patch(
-                    typeof(PerformanceScannerSet),
-                    nameof(PerformanceScannerSet.Method_Public_IEnumerator_GameObject_AvatarPerformanceStats_MulticastDelegateNPublicSealedBoCoUnique_0),
-
-                    typeof(NoPerformanceStats),
-                    nameof(CalculatePerformance),
-
-                    BindingFlags.Public | BindingFlags.Instance,
-                    BindingFlags.NonPublic | BindingFlags.Static
-                );
-            }
-            catch (Exception e) { Logger.Error("Failed to patch Performance Scanner: " + e); }
-        }
-
-        private static bool CalculatePerformance()
+        public static bool CalculatePerformance()
             => allowPerformanceScanner;
-
-        private void Patch(Type targetClass, string target, Type detourClass, string detour, BindingFlags targetBindingFlags = BindingFlags.Public | BindingFlags.Instance, BindingFlags detourBindingFlags = BindingFlags.NonPublic | BindingFlags.Static)
-            => HarmonyInstance.Patch(targetClass.GetMethod(target, targetBindingFlags), new HarmonyMethod(detourClass.GetMethod(detour, detourBindingFlags)), null, null);
-
-        private IEnumerator UiManagerInitializer()
-        {
-            while (VRCUiManager.prop_VRCUiManager_0 == null) yield return null;
-            OnUiManagerInit();
-        }
     }
 }
